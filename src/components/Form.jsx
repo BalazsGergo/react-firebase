18ca4ce2993fef4c8e51142058d8349162ea169b
Balazs Gergo <gergobalazs98@gmail.com>
2024-01-22 12:04:11 +0100

    added register/login

---
+++ src/components/Form.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { app } from "../firebase.config"
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+
+function Form({title}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleAction = () => {
+        const auth = getAuth(app)
+
+       if( title === 'login') {
+        console.log('login:',email, password)
+       } else if (title === 'register') {
+        console.log('register:', email, password )
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(res => {
+            console.log(res)
+            sessionStorage.setItem('token', res._tokenResponse.refreshToken)
+        })
+}
+}
+
+
+  return (
+    <form>
+        <h3>{title}</h3>
+
+        <input type="text" autoComplete="off" placeholder="e-mail" onChange={event => setEmail(event.target.value)} />
+        <input type="password" autoComplete="off" placeholder="password" onChange={event => setPassword(event.target.value)}/>
+
+        <button type="button" onClick={handleAction}>{title}</button>
+    </form>
+  )
+}
+
+export default Form
